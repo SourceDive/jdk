20364c07e4915900c6db2574b83b0b052a7c6294
ongoing-z <code@ongoingblog.org>
2025-07-23 12:54:52 +0800

    添加测试程序。

---
+++ my-test/JdkDemo.java
@@ -0,0 +1,10 @@
+/**
+ * @author zero
+ * @description todo
+ * @date 2025-06-14
+ */
+public class JdkDemo {
+    public static void main(String[] args) {
+        System.out.println("hello, jdk");
+    }
+}
